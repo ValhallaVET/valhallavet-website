1f54ec681ed7d7c93fead47495abfeb30d6930dc
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-09-01 12:31:45 -0400

    Fix mobile

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
+import Media from 'react-media';
 
 import Stage from './components/Stage';
 import './App.css';
 
+const StyledApp = styled.div`
+  align-items: center;
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  flex-direction: column;
+  width: 100vw;
+
+  @media all and (max-width: 960px) {
+    background: #0F5AD9;
+    padding: 10vw;
+  }
+`;
+
 const Title = styled.h2`
   color: #0F5AD9;
   font-family: 'Josefin Sans', sans-serif;
-  font-size: 2vw;
+  font-size: 2vmax;
   letter-spacing: 16px;
   margin: 0;
   margin-bottom: auto;
   text-transform: uppercase;
   width: 100%;
+
+  @media all and (max-width: 960px) {
+    color: #ffffff;
+    font-size: 6vw;
+    line-height: 1.5em;
+  }
 `;
 
 const Footer = styled.footer`
@@ -20,6 +41,12 @@ const Footer = styled.footer`
   text-align: center;
   position: relative;
   z-index: 1;
+
+  @media all and (max-width: 960px) {
+    p {
+      color: #ffffff;
+    }
+  }
 `;
 
 const Links = styled.div`
@@ -32,6 +59,10 @@ const Link = styled.a`
   font-family: 'Muli', sans-serif;
   font-weight: 600;
   position: relative;
+
+  @media all and (max-width: 960px) {
+    color: #ffffff;
+  }
 
   &:after {
     background: #7D8794;
@@ -46,6 +77,10 @@ const Link = styled.a`
     margin-left: auto;
     margin-right: auto;
     transition: all .08s linear;
+
+    @media all and (max-width: 960px) {
+      background: #ffffff;
+    }
   }
 
   &:hover {
@@ -54,6 +89,14 @@ const Link = styled.a`
     &:after {
       background: #0F5AD9;
       width: 100%;
+
+      @media all and (max-width: 960px) {
+        background: #140A5E;
+      }
+    }
+
+    @media all and (max-width: 960px) {
+      color: #140A5E;
     }
   }
 `
@@ -61,8 +104,10 @@ const Link = styled.a`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Stage />
+      <StyledApp>
+        <Media query="(min-width: 960px)" render={() => (
+          <Stage />
+        )} />
         <Footer>
           <Title>Valhalla Awaits</Title>
           <p>We’re the unofficial community-driven development community for VeChain. <br />We build sites, tools, and dapps for the benefit of the VeChain ecosystem. <br />Please join us and let’s build something incredible together</p>
@@ -70,7 +115,7 @@ class App extends Component {
             <Link href="https://github.com/ValhallaVET">Github</Link>
           </Links>
         </Footer>
-      </div>
+      </StyledApp>
     );
   }
 }
